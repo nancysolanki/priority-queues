7aecf67fa0cf3f1762e4a3f2a63bc3eb83720373
Nancy Solanki <56017974+nancysolanki@users.noreply.github.com>
2021-01-04 19:40:25 +0530

    Update k smallest .cpp

--- k smallest .cpp
+++ k smallest .cpp
@@ -1,1 +1,29 @@
 
+#include<queue>
+vector<int> kSmallest(int arr[], int n, int k) 
+{
+   priority_queue <int> pq;
+    pq.push(arr[0]);
+    for(int i=1;i<n;i++)
+    {
+        if(pq.size()==k)
+        {
+          if(pq.top()> arr[i])
+          {
+              pq.pop();
+              pq.push(arr[i]);
+          }
+            
+        }
+            else pq.push(arr[i]);
+        
+    }
+    vector<int> ans; 
+    for(int i=k;i>0;i--)
+    {
+         ans.push_back(pq.top());
+        pq.pop();
+   
+    }
+    return ans;
+}
